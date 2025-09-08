9926d31080f252d4b836ebcdbf85784d7044e20f
Farid Mahmudov <razormad666@gmail.com>
2025-09-08 18:13:16 +0300

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
-// Убираем dotenv для production, используем только переменные окружения
 export const config = {
   port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
-  allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:3001'],
+  allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['*'],
   cacheTTL: process.env.CACHE_TTL ? parseInt(process.env.CACHE_TTL, 10) : 60000
 };
