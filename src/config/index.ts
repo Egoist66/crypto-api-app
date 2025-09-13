ed4db5cd97f60bc7ccf536d9ef52e33e98347650
Farid Mahmudov <razormad666@gmail.com>
2025-09-13 18:59:29 +0300

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -25,7 +25,7 @@ export const corsOptions: CorsOptions = {
 
 export const config = {
   port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
-  allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:3000', 'http://localhost:3001', 'http://razormxc.beget.tech'],
+  allowedOrigins: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:3000', 'http://localhost:3001', 'http://razormxc.beget.tech', 'https://localhost:4000'],
   cacheTTL: process.env.CACHE_TTL ? parseInt(process.env.CACHE_TTL, 10) : 60000
 };
 
